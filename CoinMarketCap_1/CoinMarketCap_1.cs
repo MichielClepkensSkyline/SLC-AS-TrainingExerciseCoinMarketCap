86979fb9a6611739105a2b9ab98b133d1df18dcd
almabasic1 <alma.basic@skyline.be>
2025-05-29 10:29:52 +0200

    Removed and sorted usings.

--- CoinMarketCap_1/CoinMarketCap_1.cs
+++ CoinMarketCap_1/CoinMarketCap_1.cs
@@ -51,16 +51,12 @@ DATE		VERSION		AUTHOR			COMMENTS
 
 namespace CoinMarketCap_1
 {
-	using System;
-	using System.Collections.Generic;
-	using System.Globalization;
-	using System.Text;
-	using Skyline.DataMiner.Automation;
-	
-	/// <summary>
-	/// Represents a DataMiner Automation script.
-	/// </summary>
-	public class Script
+    using Skyline.DataMiner.Automation;
+
+    /// <summary>
+    /// Represents a DataMiner Automation script.
+    /// </summary>
+    public class Script
 	{
 		/// <summary>
 		/// The script entry point.
