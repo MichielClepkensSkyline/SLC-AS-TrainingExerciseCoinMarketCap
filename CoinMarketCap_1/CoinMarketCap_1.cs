ea69cb60a0b6068343f726ce710b02e789be0da3
LukaPA <luka.pavlovic@skyline.be>
2025-06-02 11:12:44 +0200

    Creating folder path from input parameter, retrieving elements with right protocol name and some testing

--- CoinMarketCap_1/CoinMarketCap_1.cs
+++ CoinMarketCap_1/CoinMarketCap_1.cs
@@ -51,16 +51,16 @@ DATE		VERSION		AUTHOR			COMMENTS
 
 namespace CoinMarketCap_1
 {
-	using System;
-	using System.Collections.Generic;
-	using System.Globalization;
-	using System.Text;
-	using Skyline.DataMiner.Automation;
-	
-	/// <summary>
-	/// Represents a DataMiner Automation script.
-	/// </summary>
-	public class Script
+    using System;
+    using System.Linq;
+    using Skyline.DataMiner.Automation;
+    using Skyline.DataMiner.Core.DataMinerSystem.Automation;
+    using Skyline.DataMiner.Net.Helper;
+
+    /// <summary>
+    /// Represents a DataMiner Automation script.
+    /// </summary>
+    public class Script
 	{
 		/// <summary>
 		/// The script entry point.
@@ -68,7 +68,64 @@ namespace CoinMarketCap_1
 		/// <param name="engine">Link with SLAutomation process.</param>
 		public void Run(IEngine engine)
 		{
-	
-		}
-	}
+            try
+            {
+                RunSafe(engine);
+            }
+            catch (ScriptAbortException)
+            {
+                // Catch normal abort exceptions (engine.ExitFail or engine.ExitSuccess)
+                throw; // Comment if it should be treated as a normal exit of the script.
+            }
+            catch (ScriptForceAbortException)
+            {
+                // Catch forced abort exceptions, caused via external maintenance messages.
+                throw;
+            }
+            catch (ScriptTimeoutException)
+            {
+                // Catch timeout exceptions for when a script has been running for too long.
+                throw;
+            }
+            catch (InteractiveUserDetachedException)
+            {
+                // Catch a user detaching from the interactive script by closing the window.
+                // Only applicable for interactive scripts, can be removed for non-interactive scripts.
+                throw;
+            }
+            catch (Exception e)
+            {
+                engine.ExitFail("Run|Something went wrong: " + e);
+            }
+        }
+
+		private void RunSafe(IEngine engine)
+        {
+            string folderPath = FolderPath(engine);
+            var dms = engine.GetDms();
+            var httpCoinMarketCapElements = dms.GetElements().
+                Where(element => element.Protocol.Name.
+                Contains("Exercise HTTP CoinMarketCap"));
+            if (httpCoinMarketCapElements.IsNullOrEmpty())
+            {
+                engine.ExitFail($"Elements with protocol name \"Exercise HTTP CoinMarketCap\" doesn't exist.");
+            }
+
+            foreach (var element in httpCoinMarketCapElements)
+            {
+                var table = element.GetTable(100);
+                var vrijednosti = table.GetData(2).Values;
+                foreach (var vri in vrijednosti)
+                {
+                    engine.GenerateInformation($"DObri dan: {vri.GetValue(2)}");
+                }
+            }
+        }
+
+		private string FolderPath(IEngine engine)
+        {
+            string folderName = engine.GetScriptParam(10).Value;
+            return $"C:\\Skyline DataMiner\\Documents\\{folderName}";
+        }
+    }
 }
