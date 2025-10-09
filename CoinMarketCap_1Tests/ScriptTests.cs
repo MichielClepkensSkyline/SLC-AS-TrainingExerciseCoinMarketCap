6be2799dc82e8054c1c560a7f1785675e03b8201
sofiankerkhof <sofian.kerkhof@skyline.be>
2025-10-09 16:17:42 +0200

    Added Unit test.

---
+++ CoinMarketCap_1Tests/ScriptTests.cs
@@ -0,0 +1,26 @@
+﻿namespace CoinMarketCap_1.Tests
+{
+	/// <summary>
+	/// Unit Test Class.
+	/// </summary>
+	[TestClass]
+	public class ScriptTests
+	{
+		/// <summary>
+		/// Test method for FillRecords where tabledata is null.
+		/// </summary>
+		[TestMethod]
+		public void FillRecordsTest()
+		{
+			// Arrange
+			Dictionary<string, object[]>? tabledata = null;
+			Script script = new Script();
+
+			// Act
+			List<TableRow> result = script.FillRecords(tabledata);
+
+			// Assert
+			Assert.IsTrue(result.Count == 0);
+		}
+	}
+}
