6231a95bc6350be89b00fd618409994e4a05f39b
emirburic1 <emir.buric@skyline.be>
2025-10-09 12:29:44 +0200

    Added the data storing of the latest listings table into the csv file

--- CoinMarketCap_1/Data.cs
+++ CoinMarketCap_1/Data.cs
@@ -1,9 +1,12 @@
-﻿using Skyline.DataMiner.Automation;
+﻿using CsvHelper;
+using Skyline.DataMiner.Automation;
 using Skyline.DataMiner.Core.DataMinerSystem.Common;
 using Skyline.DataMiner.Core.DataMinerSystem.Common.Selectors;
 using Skyline.DataMiner.Net.ReportsAndDashboards;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,11 +15,16 @@ namespace CoinMarketCap_1
 {
     public class Data
     {
+        public enum Tables
+        {
+            LatestListings = 100,
+            Categories = 400,
+        }
+
         public void GetData(IDms dms, IEngine engine)
         {
             string protocolName = "Exercise HTTP CoinMarketCap Emir";
             string protocolVersion = "Production";
-
 
             if (!dms.ProtocolExists(protocolName, protocolVersion))
             {
@@ -24,29 +32,26 @@ namespace CoinMarketCap_1
                 return;
             }
 
-            IEnumerable<IDmsElement> elements = dms.GetElements().Where(e => e.Protocol.Name == protocolName && e.Protocol.Version== protocolVersion);
+            IEnumerable<IDmsElement> elements = dms.GetElements().Where(e => e.Protocol.Name == protocolName && e.Protocol.Version == protocolVersion);
             foreach (IDmsElement element in elements)
             {
                 try
                 {
-                    int latestListingsTableId = 100;
-                    var latestListingsData= ReadDataFormTable(latestListingsTableId, element, engine);
+                    var latestListingsData= ReadDataFormTable((int)Tables.LatestListings, element, engine);
                     if(latestListingsData != null)
                     {
-                        StoreDataInCSV(latestListingsData, engine);
+                        StoreDataInCSV(latestListingsData, engine,element.Name);
                     }
 
-                    int categoriesTableId = 400;
-                    ReadDataFormTable(categoriesTableId, element, engine);
-                    var categoriesData = ReadDataFormTable(categoriesTableId, element, engine);
+                    var categoriesData = ReadDataFormTable((int)Tables.Categories, element, engine);
                     if (categoriesData != null)
                     {
-                        StoreDataInCSV(categoriesData, engine);
+                        // StoreDataInCSV(categoriesData, engine);
                     }
                 }
                 catch (Exception ex)
                 {
-                    engine.Log($"[ERROR] Failed to get store data from {element.Name}: {ex.Message}");
+                    engine.Log($"[ERROR] Failed to get and store data from {element.Name}: {ex.Message}");
                 }
             }
         }
@@ -70,9 +75,57 @@ namespace CoinMarketCap_1
             return tableData;
         }
 
-        public void StoreDataInCSV(IDictionary<string, object[]> tableData, IEngine engine)
+        public void StoreDataInCSV(IDictionary<string, object[]> tableData, IEngine engine, string elementName)
         {
+            string filePath = $"C:\\Skyline DataMiner\\Documents\\SLC-AS-TrainingExerciseCoinMarketCap\\{elementName}.csv";
 
+            using (var writer = new StreamWriter(filePath))
+            using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+            {
+                List<LatestListings> listings = new List<LatestListings>();
+
+                foreach (var row in tableData)
+                {
+                    object[] cols = row.Value;
+                    engine.Log($"Date: {cols[9]}");
+                    try
+                    {
+                        listings.Add(new LatestListings
+                        {
+                            Id = row.Key,
+                            Symbol = cols[2]?.ToString(),
+                            Name = cols[1]?.ToString(),
+                            CoinMarketCapRank = Convert.ToInt32(cols[3]),
+                            CirculatingSupply = Convert.ToDouble(cols[4]),
+                            PriceInUSD = Convert.ToDouble(cols[5]),
+                            MarketCapInUSD = Convert.ToDouble(cols[6]),
+                            OneHourChange = Convert.ToDouble(cols[7]),
+                            OneDayVolumeChange = Convert.ToDouble(cols[8]),
+                            LastUpdated = DateTime.FromOADate(Convert.ToDouble(cols[9])),
+                            DisplayKey = cols[10]?.ToString(),
+                        });
+                    }
+                    catch (Exception ex)
+                    {
+                        engine.Log($"[ERROR] Failed to map row '{row.Key}': {ex.Message}");
+                    }
+                }
+
+                foreach(var listing in listings)
+                {
+                    engine.Log($"ID: {listing.Id}");
+                    engine.Log($"Name: {listing.Id}");
+                    engine.Log($"Rank: {listing.Id}");
+                    engine.Log($"Price in USD: {listing.Id}");
+                    engine.Log($"Market Cap: {listing.Id}");
+                    engine.Log($"One Hour Change: {listing.Id}");
+                    engine.Log($"One Day Volume: {listing.Id}");
+                    engine.Log($"Last Updated: {listing.Id}");
+                    engine.Log($"Display Key: {listing.DisplayKey}");
+                }
+
+                csv.WriteRecords(listings);
+            }
         }
     }
 }
---
+++ CoinMarketCap_1/LatestListings.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoinMarketCap_1
+{
+    public class LatestListings
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Symbol { get; set; }
+
+        public int CoinMarketCapRank { get; set; }
+
+        public double CirculatingSupply { get; set; }
+
+        public double PriceInUSD { get; set; }
+
+        public double MarketCapInUSD { get; set; }
+
+        public double OneHourChange { get; set; }
+
+        public double OneDayVolumeChange { get; set; }
+
+        public DateTime LastUpdated { get; set; }
+
+        public string DisplayKey { get; set; }
+    }
+}
