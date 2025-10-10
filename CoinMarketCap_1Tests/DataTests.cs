f6b6f2c35697549c85b7e1bcbd703899eb137431
emirburic1 <emir.buric@skyline.be>
2025-10-10 11:37:52 +0200

    Added tests for the ReadDataFromTable method

--- CoinMarketCap_1Tests/DataTests.cs
+++ CoinMarketCap_1Tests/DataTests.cs
@@ -13,8 +13,8 @@ namespace CoinMarketCap_1.Tests
 	using Microsoft.VisualStudio.TestTools.UnitTesting;
 	using Moq;
 	using Skyline.DataMiner.Automation;
-    using Skyline.DataMiner.Core.DataMinerSystem.Common;
-    using Skyline.DataMiner.Core.DataMinerSystem.Common.Selectors;
+	using Skyline.DataMiner.Core.DataMinerSystem.Common;
+	using Skyline.DataMiner.Core.DataMinerSystem.Common.Selectors;
 
 	[TestClass]
 	public class DataTests
@@ -84,6 +84,57 @@ namespace CoinMarketCap_1.Tests
 			// Assert
 			Assert.IsNull(result);
 			this.mockEngine.Verify(e => e.Log("[ERROR] Table with the designated id was not found in MockElement"), Times.Once);
+		}
+
+		[TestMethod]
+		public void ReadDataFromTable_TableDataIsNull()
+		{
+			// Arrange
+			var mockTable = new Mock<IDmsTable>();
+			mockTable.Setup(t => t.GetData(It.IsAny<int>())).Returns((IDictionary<string, object[]>)null);
+
+			var mockElement = new Mock<IDmsElement>();
+			mockElement.Setup(e => e.Name).Returns("MockElement");
+			mockElement.Setup(e => e.GetTable(It.IsAny<int>())).Returns(mockTable.Object);
+
+			var data = new Data();
+
+			// Act
+			var result = data.ReadDataFormTable(100, mockElement.Object, this.mockEngine.Object);
+
+			// Assert
+			Assert.IsNull(result);
+			this.mockEngine.Verify(e => e.Log("[ERROR] No data was found in the table with the deisgnated id in the MockElement element"), Times.Once);
+		}
+
+		public void ReadDataFormTable_ValidTableAndData_ReturnsData()
+		{
+			// Arrange
+			var expectedData = new Dictionary<string, object[]>
+			{
+				{ "1", new object[] { "BTC", 50000.0 } },
+				{ "2", new object[] { "ETH", 4000.0 } },
+			};
+
+			var mockTable = new Mock<IDmsTable>();
+			mockTable.Setup(t => t.GetData(It.IsAny<int>())).Returns((IDictionary<string, object[]>)null);
+
+			var mockElement = new Mock<IDmsElement>();
+			mockElement.Setup(e => e.GetTable(It.IsAny<int>())).Returns(mockTable.Object);
+			mockElement.Setup(e => e.Name).Returns("MockElement");
+
+			var mockEngine = new Mock<IEngine>();
+
+			var data = new Data();
+
+			// Act
+			var result = data.ReadDataFormTable(100, mockElement.Object, mockEngine.Object);
+
+			// Assert
+			Assert.IsNotNull(result);
+			Assert.AreEqual(2, result.Count);
+			Assert.AreEqual("BTC", result["1"][0]);
+			mockEngine.Verify(e => e.Log(It.IsAny<string>()), Times.Never);
 		}
 	}
 }
